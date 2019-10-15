c716a82c216b33e663eb9b04f5c7ca76d051ad2c
GENREV ARAMBULO (SAPE-MIS-ISD-OOCLL/MNL) <ARAMBGE@OOCL.COM>
2019-10-15 21:18:27 +0800

    Changed data type of employee id to int

--- src/main/java/com/tw/apistackbase/model/Employee.java
+++ src/main/java/com/tw/apistackbase/model/Employee.java
@@ -1,19 +1,19 @@
 package com.tw.apistackbase.model;
 
 public class Employee {
-    private String id;
+    private int id;
     private String name;
     private int age;
     private String gender;
 
-    public Employee(String id, String name, int age, String gender) {
+    public Employee(int id, String name, int age, String gender) {
         this.id = id;
         this.name = name;
         this.age = age;
         this.gender = gender;
     }
 
-    public String getId() {
+    public int getId() {
         return id;
     }
 
