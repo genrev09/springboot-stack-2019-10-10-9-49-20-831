fb0f3225b8d9e09f08274e55187368c2bff4e25f
GENREV ARAMBULO (SAPE-MIS-ISD-OOCLL/MNL) <ARAMBGE@OOCL.COM>
2019-10-15 21:20:13 +0800

    Added Employee Controller with add employees method

---
+++ src/main/java/com/tw/apistackbase/controller/EmployeeController.java
@@ -0,0 +1,25 @@
+package com.tw.apistackbase.controller;
+
+import com.tw.apistackbase.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("employee")
+public class EmployeeController {
+
+    private List<Employee> employeeList = new ArrayList<>();
+
+    @PostMapping(path = "/addEmployees", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<String> addEmployees(@RequestBody List<Employee> employees){
+        boolean isAdded = employeeList.addAll(employees);
+        if(isAdded)
+            return ResponseEntity.ok("Employees Added!");
+        else
+            return ResponseEntity.ok("Failed to add employees");
+    }
+}
